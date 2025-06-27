c1860dd671ac50c6996ae6749da526fea6365a0e
Sindre Østrem <sindre.ostrem@nb.no>
2025-06-27 16:22:16 +0200

    add more configs (wezterm, helix)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -24,6 +24,12 @@
     obsidian
     fzf
     zoxide
+    lazygit
+    kotlin-language-server
+    lua-language-server
+    marksman
+    nil
+    nixpkgs-fmt
   ];
   programs.home-manager.enable = true;
   nixGL.packages = import nixgl { inherit pkgs; };
@@ -34,12 +40,14 @@
     enable = true;
     package = (config.lib.nixGL.wrap inputs.wezterm.packages.${pkgs.system}.default);
   };
-  #home.file.".config/wezterm/wezterm.lua".source = ./wezterm.lua;
+  home.file.".config/wezterm/wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink /home/sindreo/nix/home-manager/dotfiles/wezterm.lua;
+  home.file.".config/helix/config.toml".source = config.lib.file.mkOutOfStoreSymlink /home/sindreo/nix/home-manager/dotfiles/helix/config.toml;
+  home.file.".config/helix/languages.toml".source = config.lib.file.mkOutOfStoreSymlink /home/sindreo/nix/home-manager/dotfiles/helix/languages.toml;
   home.shellAliases = {
     k = "kubectl";
   };
   home.sessionVariables = {
-    EDITOR = "nvim";
+    EDITOR = "hx";
     BROWSER = "firefox";
   };
 }
