25b133fd2b6ccd38066e31bfa5956daabe6f8aae
Sindre Østrem <sindre.ostrem@nb.no>
2025-07-04 16:19:37 +0200

    yazi y shell wrapper

--- home-manager/dotfiles/config/fish/config.fish
+++ home-manager/dotfiles/config/fish/config.fish
@@ -8,3 +8,11 @@ alias ls="eza --icons --group-directories-first"
 alias tree="eza --tree --icons --group-directories-first"
 zoxide init --cmd cd fish | source
 starship init fish | source
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
